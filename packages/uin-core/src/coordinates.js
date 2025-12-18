53c17f473787bb1851759e56a3ec5dc0f5e40af3
N0Nzense <99034586+z3uss3l@users.noreply.github.com>
2025-12-18 04:47:44 +0100

    Create coordinates.js

---
+++ packages/uin-core/src/coordinates.js
@@ -0,0 +1,19 @@
+export function worldToNormalized(point, bounds) {
+  return {
+    x: (point.x - bounds.x[0]) / (bounds.x[1] - bounds.x[0]),
+    y: 1 - (point.y - bounds.y[0]) / (bounds.y[1] - bounds.y[0]),
+    z: (point.z - bounds.z[0]) / (bounds.z[1] - bounds.z[0])
+  };
+}
+
+export function normalizedToScreen(norm, viewport) {
+  return {
+    x: norm.x * viewport.width,
+    y: norm.y * viewport.height,
+    z: norm.z
+  };
+}
+
+export function worldToScreen(point, bounds, viewport) {
+  return normalizedToScreen(worldToNormalized(point, bounds), viewport);
+}
