b4ed4254b5e2f60bb3d88f9757d9a5ef0dce1809
N0Nzense <99034586+z3uss3l@users.noreply.github.com>
2025-12-18 05:03:03 +0100

    Update index.js

--- packages/uin-core/src/index.js
+++ packages/uin-core/src/index.js
@@ -2,3 +2,6 @@ export * from './parser.js';
 export * from './validator.js';
 export * from './objectTypes.js';
 export * from './coordinates.js';
+export * from './renderHelpers.js';
+export * from './renderer.js';
+export * from './depthMapGenerator.js';
